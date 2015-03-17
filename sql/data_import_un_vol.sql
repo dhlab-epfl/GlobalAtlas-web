08e04ebe67ada21a2a98d8659f40f4ef53f348ea
olivierdalang <olivier.dalang@epfl.ch>
2015-03-17 16:57:38 +0100

    fix un vol import

--- sql/data_import_un_vol.sql
+++ sql/data_import_un_vol.sql
@@ -21,24 +21,26 @@ WHERE NOT EXISTS ( SELECT id FROM vtm.entity_types WHERE name = 'building' );
 /*******************************/
 
 INSERT INTO vtm.entities (name, type_id)
-SELECT 	'un_vol_entity_'||id as name,
+SELECT 	'un_vol_entity_'||source.id_0 as name,
 		(SELECT id FROM vtm.entity_types WHERE name='building') as type_id
 FROM 	"data_external"."un_vol" as source
-WHERE 	ST_Intersects(source.geom, ST_Transform(ST_MakeEnvelope(12.3328,45.4365,12.3401,45.4401, 4326),3004));
+WHERE 	ST_Intersects(source.geom, ST_MakeEnvelope(2311190,5034958,2311882,5035316,3004) );
 
 
 /*******************************/
 /* EVENTS (geom)               */
 /*******************************/
 
-INSERT INTO vtm.events(entity_id, key, geovalue, date, source_id)
-SELECT 	(SELECT id FROM vtm.entities WHERE name=('un_vol_entity_'||source.id) LIMIT 1) as entity_id,
-		'geom' as key,
+INSERT INTO vtm.properties(entity_id, property_type_id, geovalue, date, source_id)
+SELECT 	ent.id as entity_id,
+		0 as property_type_id,
 		ST_Transform(source.geom,4326) as geovalue,
 		2015 as date,
 		(SELECT id FROM vtm.sources WHERE name='UN_VOL') as source_id
 FROM 	"data_external"."un_vol" as source
-WHERE 	ST_Intersects(source.geom, ST_Transform(ST_MakeEnvelope(12.3328,45.4365,12.3401,45.4401, 4326),3004));
+LEFT JOIN 	vtm.entities as ent ON ent.name=('un_vol_entity_'||source.id_0)
+WHERE 	ST_Intersects(source.geom, ST_MakeEnvelope(2311190,5034958,2311882,5035316,3004));
+
 
 
 
@@ -46,13 +48,13 @@ WHERE 	ST_Intersects(source.geom, ST_Transform(ST_MakeEnvelope(12.3328,45.4365,1
 /* EVENTS (height              */
 /*******************************/
 
-INSERT INTO vtm.entity_types(name) VALUES ('height');
+INSERT INTO vtm.properties_types(name) VALUES ('height');
 
-INSERT INTO vtm.events(entity_id, key, value, date, source_id)
-SELECT 	(SELECT id FROM vtm.entities WHERE name=('un_vol_entity_'||source.id) LIMIT 1) as entity_id,
-		'height' as key,
+INSERT INTO vtm.properties(entity_id, property_type_id, value, date, source_id)
+SELECT 	(SELECT id FROM vtm.entities WHERE name=('un_vol_entity_'||source.id_0) LIMIT 1) as entity_id,
+		(SELECT id FROM vtm.properties_types WHERE name='height') as property_type_id,
 		uv_qcolmo as value,
 		2015 as date,
 		(SELECT id FROM vtm.sources WHERE name='UN_VOL') as source_id
 FROM 	"data_external"."un_vol" as source
-WHERE 	ST_Intersects(source.geom, ST_Transform(ST_MakeEnvelope(12.3328,45.4365,12.3401,45.4401, 4326),3004));
+WHERE 	ST_Intersects(source.geom, ST_MakeEnvelope(2311190,5034958,2311882,5035316,3004));
